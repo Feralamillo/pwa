3e2be09d8529f369c6ab77e250082506c96f572d
Feralamillo <9921230+Feralamillo@users.noreply.github.com>
2022-04-06 08:51:37 +0100

    [PWA] custom installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { useAddToHomescreenPrompt } from './hooks/useAddToHomescreenPrompt';
 
 function App() {
+  const [prompt, promptToInstall] = useAddToHomescreenPrompt();
+  const [isVisible, setVisibleState] = React.useState(false);
+
+  useEffect(() => {
+    if (prompt) {
+      setVisibleState(true);
+    }
+  }, [prompt]);
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        {isVisible && (
+          <Button variant='contained' color='primary' onClick={promptToInstall}>
+            Add to Homescreen
+          </Button>
+        )}
       </header>
     </div>
   );
